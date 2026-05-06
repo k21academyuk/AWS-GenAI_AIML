b5daa743492eb9db01b3753d9805d4cfc3dcb44c
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2026-05-06 14:11:09 +0530

    Add AWS and SageMaker configuration to config.py

--- fraud-detection-lab/package/config.py
+++ fraud-detection-lab/package/config.py
@@ -1,1 +1,10 @@
+import boto3
+import sagemaker
 
+AWS_REGION           = boto3.session.Session().region_name
+SAGEMAKER_IAM_ROLE   = sagemaker.get_execution_role()
+MODEL_DATA_S3_BUCKET = sagemaker.Session().default_bucket()
+SOLUTION_NAME        = 'fraud-detection-using-machine-learning'
+SOLUTIONS_S3_BUCKET  = 'sagemaker-solutions-prod'
+SOLUTION_PREFIX      = 'sagemaker-soln-fdml'
+SAGEMAKER_MODE       = 'Studio'
